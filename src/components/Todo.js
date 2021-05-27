1e4305ec0601941193dcaa3b5ec9095e3a2264cd
EvgenPrushk <ogmprushak@mail.ru>
2021-05-27 14:18:15 +0300

    feat: add MainTodo and TodoScreen

---
+++ src/components/Todo.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+export const Todo = ({ todo, onRemove }) => {
+  // delete item
+  const longPressHandler = () => {
+    onRemove(todo.id);
+  };
+
+  return (
+    <TouchableOpacity
+      activeOpacity={0.5}
+      onPress={() => console.log("Pressed", todo.id)}
+      onLongPress={longPressHandler}
+      // firs param - context, second param  -todo.id
+      // onLongPress={onRemove.bind(todo.id)}
+      
+    >
+      <View style={styles.todo}>
+        <Text>{todo.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  todo: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 15,
+    borderWidth: 1,
+    borderColor: "#eee",
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+});
