052bc0b88d9df6b797ed62bea840efa76635a4b5
EvgenPrushk <ogmprushak@mail.ru>
2021-05-27 16:42:58 +0300

    feat:  add object for color

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,13 +1,33 @@
 import React from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
+import { THEME } from "../theme";
 
 export const TodoScreen = ({ goBack, todo }) => {
   return (
     <View>
       <Text>{todo.title}</Text>
-      <Button title="Назад" onPress={goBack} />
+      <View style={styles.buttons}>
+        <View style={styles.button}>
+          <Button title="Назад" onPress={goBack} color={THEME.GRAY_COLOR} />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="Delete"
+            color={THEME.DANGER_COLOR}
+            onPress={() => console.log("Delete")}
+          />
+        </View>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  button: {
+    width: "40%",
+  },
+});
