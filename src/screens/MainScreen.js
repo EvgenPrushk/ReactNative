1f518244c6b149758a75462c6aa3f06c69ad1764
Evgen Prushak <ogmprushak@mail.ru>
2021-06-02 22:49:07 +0300

    fix: source

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -19,7 +19,7 @@ export const MainScreen = ({ addTodo, todos, removeTodo, openTodo }) => {
       <View style={styles.imgWrap}>
         <Image
           style={styles.imgageNoItem}
-          source={require = "../../assets/no-items.png"}         
+          source={require = ("../../assets/no-items.png")}         
         />
       </View>
     );
