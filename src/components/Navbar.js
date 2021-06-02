e018d45ad53f271d6892d10f74d8676b6ad7069b
Evgen Prushak <ogmprushak@mail.ru>
2021-06-02 22:32:42 +0300

    refactor: Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { THEME } from "../theme";
-export const Navbar = ({title}) => {
-  
+
+export const Navbar = ({ title }) => {
   return (
     <View style={styles.navbar}>
       <Text style={styles.text}>{title}</Text>
     </View>
   );
-  
 };
 
 const styles = StyleSheet.create({
@@ -20,7 +19,7 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
   },
   text: {
-      color: "white",
-      fontSize: 10,
+    color: "white",
+    fontSize: 10,
   },
 });
