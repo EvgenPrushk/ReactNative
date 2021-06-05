c8cdb6912d6798a035e0dfdcb71d3c3852ea505e
Evgen Prushak <ogmprushak@mail.ru>
2021-06-05 09:49:19 +0300

    feat: add context for Reducer

---
+++ src/context/screen/ScreenState.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ScreenContext } from "./screenContext";
+import { ScreenReducer } from "./screenReducer";
+
+export const ScreenState = ({ children }) => {
+  const [state, dispatch] = useReducer(ScreenReducer, null);
+
+  const changeScreen = (id) => dispatch({ type: CHANGE_SCREEN, payload: id });
+  return (
+    <ScreenContext.Provider value={{ changeScreen, todoId: state }}>
+      {children}
+    </ScreenContext.Provider>
+  );
+};
---
+++ src/context/screen/screenReducer.js
@@ -0,0 +1,3 @@
+export const screenReducer = (action, state) => {
+  return state;
+};
