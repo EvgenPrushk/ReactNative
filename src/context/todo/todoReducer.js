54bb17c9f20d1cc9a2b94decb5f223f8ebda16e7
Evgen Prushak <ogmprushak@mail.ru>
2021-06-05 09:17:02 +0300

    feat: refactor todoReducer

--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,40 +1,41 @@
 import { ADD_TODO, UPDATE_TODO, REMOVE_TODO } from "../types";
 
-export const todoReducer = (state, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return {
-        // ...state -deploy state
-        ...state,
-        todos: [
-          //todos : [...] - copy now state and add new Object (id + title PARAMS)
-          ...state.todos,
-          {
-             // key is String in React
-            id: Date.now().toString(),
-            title: action.title,
-          },
-        ],
-      };
-    case UPDATE_TODO:
-      return {
-        // ...state -deploy state
-        ...state,
-        // filter todo.id !== action.id
-        todos: state.todos.filter((todo) => todo.id !== action.id),
-      };
-    case REMOVE_TODO:
-      return {
-        ...state,
-        todos: state.todos.map((todo) => {
-          if (todo.id === action.id) {
-            todo.title = action.title;
-          }
-          return todo;
-        }),
-      };
+const handlers = {
+  [ADD_TODO]: (state, { title }) => ({
+    // ...state -deploy state
+    ...state,
+    todos: [
+      //todos : [...] - copy now state and add new Object (id + title PARAMS)
+      ...state.todos,
+      {
+        // key is String in React
+        id: Date.now().toString(),
+        title,
+      },
+    ],
+  }),
 
-    default:
-      return state;
-  }
+  [REMOVE_TODO]: (state, id) => ({
+    // ...state -deploy state
+    ...state,
+    // filter todo.id !== action.id
+    todos: state.todos.filter((todo) => todo.id !== action.id),
+  }),
+
+  [UPDATE_TODO]: (state, { title, id }) => ({
+    ...state,
+    todos: state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.title = title;
+      }
+      return todo;
+    }),
+  }),
+
+  DEFAULT: (state) => state,
+};
+
+export const todoReducer = (state, action) => {
+  const handler = handlers[action.type] || handlers.DEFAULT;
+  return handler(state, action);
 };
