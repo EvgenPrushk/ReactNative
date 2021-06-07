6b1e13a8f7552b201c1f315c09ef4fff6d50a86c
Evgen Prushak <ogmprushak@mail.ru>
2021-06-07 22:13:53 +0300

    fix: main screen

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,56 +1,31 @@
-import React, { useContext } from "react";
-import { View, StyleSheet } from "react-native";
-import { Navbar } from "./components/Navbar";
-import { THEME } from "./theme";
-
-import { MainScreen } from "./screens/MainScreen";
-import { TodoScreen } from "./screens/TodoScreen";
-import { TodoContext } from "./context/todo/TodoContext";
-import { ScreenContext } from "./context/screen/screenContext";
+import React, {  useContext } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Navbar } from './components/Navbar'
+import { THEME } from './theme'
+import { MainScreen } from './screens/MainScreen'
+import { TodoScreen } from './screens/TodoScreen'
+import { ScreenContext } from './context/screen/screenContext'
 
 export const MainLayout = () => {
-  const { todos, addTodo, removeTodo, updateTodo } = useContext(TodoContext);
-  const { todoId, changeScreen } = useContext(ScreenContext);
-
-  let content = (
-    <MainScreen
-      todos={todos}
-      addTodo={addTodo}
-      removeTodo={removeTodo}
-      updateTodo={updateTodo}
-      openTodo={changeScreen}
-    />
-  );
-
-  if (todoId) {
-    // add function go to MainScreen
-    const selectedTodo = todos.find((todo) => todo.id === todoId);
-    content = (
-      <TodoScreen
-        onRemove={removeTodo}
-        goBack={() => changeScreen(null)}
-        todo={selectedTodo}
-        onSave={updateTodo}
-      />
-    );
-  }
+  const { todoId } = useContext(ScreenContext)
 
   return (
     <View style={styles.wrapper}>
-      <Navbar title="Todo App!" />
-
-      <View style={styles.container}>{content}</View>
+      <Navbar title='Todo App!' />
+      <View style={styles.container}>
+        {todoId ? <TodoScreen /> : <MainScreen />}
+      </View>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: THEME.PADDING_HORIZONTAL,
-    paddingVertical: THEME.PADDING_VERTICAL,
-    flex: 1,
+    paddingVertical: 20,
+    flex: 1
   },
   wrapper: {
-    flex: 1,
-  },
-});
+    flex: 1
+  }
+})
