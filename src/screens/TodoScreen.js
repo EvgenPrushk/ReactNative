af36c2176b4872d5bee81d4d98d3a5469590f4d8
EvgenPrushk <ogmprushak@mail.ru>
2021-06-17 13:46:54 +0300

    fix: TodoScreen

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,18 +1,23 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { View, StyleSheet, Dimensions } from "react-native";
-import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome, AntDesign } from "@expo/vector-icons";
 import { THEME } from "../theme";
 import { AppCard } from "../components/ui/AppCard";
 import { EditModal } from "../components/EditModal";
 import { AppTextBold } from "../components/ui/AppTextBold";
-import {AppButton} from '../components/ui/AppButton'
+import { AppButton } from "../components/ui/AppButton";
+import { TodoContext } from "../context/todo/todoContext";
+import { ScreenContext } from "../context/screen/screenContext";
 
-
-export const TodoScreen = ({ goBack, todo, onRemove, onSave }) => {
+export const TodoScreen = () => {
+  const { todos, updateTodo, removeTodo } = useContext(TodoContext);
+  const { todoId, changeScreen } = useContext(ScreenContext);
   const [modal, setModal] = useState(false);
 
+  const todo = todos.find((t) => t.id === todoId);
+
   const saveHandler = async (title) => {
-    await onSave(todo.id, title);
+    await updateTodo(todo.id, title);
     setModal(false);
   };
 
@@ -34,15 +39,18 @@ export const TodoScreen = ({ goBack, todo, onRemove, onSave }) => {
 
       <View style={styles.buttons}>
         <View style={styles.button}>
-          <AppButton onPress={goBack} color={THEME.GREY_COLOR}>
-            <FontAwesome name="backward" size={20} color="#fff" />
+          <AppButton
+            onPress={() => changeScreen(null)}
+            color={THEME.GREY_COLOR}
+          >
+            <AntDesign name="back" size={20} color="#fff" />
           </AppButton>
         </View>
 
         <View style={styles.button}>
           <AppButton
             color={THEME.DANGER_COLOR}
-            onPress={() => onRemove(todo.id)}
+            onPress={() => removeTodo(todo.id)}
           >
             <FontAwesome name="remove" size={20} color="#fff" />
           </AppButton>
